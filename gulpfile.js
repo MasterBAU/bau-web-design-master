a3e63b5cc7bb4ec6bbbea5b732b2d52fc897acb4
Nahuel Sotelo <nahuelsotelo@gmail.com>
2016-02-28 19:05:23 +0100

    change jekyll compilation task and source folder names

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ var onError = function(err) {
 // CLEAN ================================================
 gulp.task('clean', function(callback) {
     del(
-        [ path.tmp, path.dist + '/*' ],
+        [ path.dist + '/*' ],
         function(err, deletedFiles) {
             console.log('Files deleted:\n'.bold.green , deletedFiles.join(',\n '));
             callback();
@@ -41,64 +41,55 @@ gulp.task('clean', function(callback) {
 
 
 // HTML ==================================================
-gulp.task('jekyll', function(done) {
+  gulp.task('html', function(done) {
     return cp.spawn('bundle',
-        [
-            'exec',
-            'jekyll',
-            'build',
-            '-q',
-            '--source=' + path.src,
-            '--destination=' + path.tmp + '/jekyll',
-            '--config=_config.yml'
-        ], { stdio: 'inherit' })
-  .on('close', done);
-});
-gulp.task('copy-html', ['jekyll'], function() {
-    return gulp.src( path.tmp + '/jekyll/**/**.*')
-        .pipe(gulp.dest( path.dist ));
-});
-gulp.task('html', ['copy-html'], function() {
-    return browsersync.reload();
-});
+      [
+        'exec',
+        'jekyll',
+        'build',
+        '-q',
+        '--config=_config.yml'
+      ], { stdio: 'inherit' })
+      .on('close', done);
+  });
 
 
 // STYLES ===============================================
 gulp.task('css', function() {
-    return gulp.src( path.src + '/_scss/*.scss' )
+    return gulp.src( path.src + '/scss/*.scss' )
         .pipe(plumber({
             errorHandler: onError
         }))
         .pipe(sass())
-        .pipe(gulp.dest( path.dist + '/css' ))
+        .pipe(gulp.dest( path.dist + '/css' ));
 });
 
 
 // FONTS ================================================
 gulp.task('fonts', function() {
-    return gulp.src( path.src + '/_fonts/**/*.*' )
-        .pipe(gulp.dest( path.dist + '/fonts' ))
+    return gulp.src( path.src + '/fonts/**/*.*' )
+        .pipe(gulp.dest( path.dist + '/fonts' ));
 });
 
 
 // JS ===================================================
 gulp.task('js', function() {
     return gulp.src([
-      path.src + '/_js/reveal/*.js'
+      path.src + '/js/reveal/*.js'
     ])
-    .pipe(gulp.dest( path.dist + '/js/reveal'))
+    .pipe(gulp.dest( path.dist + '/js/reveal'));
 });
 
 
 // IMAGES ===============================================
 gulp.task('images', function() {
-    return gulp.src( path.src + '/_img/**/*.+(png|jpg|gif)' )
+    return gulp.src( path.src + '/img/**/*.+(png|jpg|gif)' )
         .pipe(changed( path.dist + '/img' ))
         .pipe(gulp.dest( path.dist + '/img' ));
 });
 
 gulp.task('svg', function() {
-    return gulp.src( path.src + '/_img/**/*.svg' )
+    return gulp.src( path.src + '/img/**/*.svg' )
         // .pipe(changed('dist/img'))
         .pipe(gulp.dest( path.dist + '/img/' ));
 });
@@ -116,17 +107,17 @@ gulp.task('browsersync', function() {
         server: { baseDir: path.dist },
         port: 8000,
         files: [  path.dist + '/css/*.css']
-    })
+    });
 });
 
 
 // WATCH ================================================
 gulp.task('watch', ['browsersync'], function() {
     gulp.watch( path.src + '/**/*.html',             ['html', 'files'] );
-    gulp.watch( path.src + '/_scss/**/*.scss',       ['css'] );
-    gulp.watch( path.src + '/_js/**/*.js',           ['js'] );
-    gulp.watch( path.src + '/_img/**/*.+(png|jpg)',  ['images'] );
-    gulp.watch( path.src + '/_img/**/*.svg',         ['svg'] );
+    gulp.watch( path.src + '/scss/**/*.scss',       ['css'] );
+    gulp.watch( path.src + '/js/**/*.js',           ['js'] );
+    gulp.watch( path.src + '/img/**/*.+(png|jpg)',  ['images'] );
+    gulp.watch( path.src + '/img/**/*.svg',         ['svg'] );
 });
 
 
